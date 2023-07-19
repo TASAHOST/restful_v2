55b8a20579b639e351abfa5a7c3f920755c7513d
TASAHOST <120707750+TASAHOST@users.noreply.github.com>
2023-07-19 09:57:15 +0700

    push

--- models/db.js
+++ models/db.js
@@ -1,16 +1,18 @@
-const mysql = require('mysql');
+const {Sequelize} = require("sequelize");
 const dbConfig = require("../config/db.config");
-
-const connection =mysql.createConnection({
+//Create sequelize instance
+const sequelize = new Sequelize(dbConfig.DB , dbConfig.USER, dbConfig.PASSWORD ,{
     host:dbConfig.HOST,
-    user:dbConfig.USER,
-    password:dbConfig.PASSWORD,
-    database:dbConfig.DB
+    dialect:"mysql"
 })
 
-connection.connect   
-    ((error)=>{
-        if(error) throw error;
-        console.log("succ");
-    });
-    module.exports= connection;
+async function testConnection(){
+  try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+}
+testConnection();
+module.exports = sequelize;
