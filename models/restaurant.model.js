7830e11ae677c56024fedc19c22d31c8ce0d4ea9
TASAHOST <120707750+TASAHOST@users.noreply.github.com>
2023-07-19 10:19:51 +0700

    succ for connect

--- models/restaurant.model.js
+++ models/restaurant.model.js
@@ -1,1 +1,32 @@
 const { DataType, DataTypes} = require("sequelize");
+const sequelize = require("./db");
+
+const Restaurant = sequelize.define("restuarant",{
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement: true
+    },
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false
+
+    },
+    Image:{
+        type:DataTypes.TEXT,
+        allowNull:false
+    },
+    createdAt:{
+        type:DataTypes.DATE,
+        allowNull:false,
+        defaultValue : Date.now
+
+    },
+    updateAt:{
+        type:DataTypes.DATE,
+        allowNull: true,
+        defaultValue :Date.now
+    }
+});
+
+module.exports = Restaurant;
