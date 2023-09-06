38f6c56f7d07a7396a88b04eb13bb1b0a77fff67
TASAHOST <120707750+TASAHOST@users.noreply.github.com>
2023-09-06 15:43:06 +0700

    online

--- models/db.js
+++ models/db.js
@@ -3,7 +3,13 @@ const dbConfig = require("../config/db.config");
 //Create sequelize instance
 const sequelize = new Sequelize(dbConfig.DB , dbConfig.USER, dbConfig.PASSWORD ,{
     host:dbConfig.HOST,
-    dialect:"mysql"
+    dialect:"mysql",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 })
 
 async function testConnection(){
