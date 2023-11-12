0053ac7ff84ea2c47a67310a34deaa20ab4abe60
TASAHOST <120707750+TASAHOST@users.noreply.github.com>
2023-11-12 16:22:18 +0700

    all Done

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     secret:"foodpandy-secret-kye",
     //ตัวใช้งาน
-    // jwtExpiration:3600,
-    // jwtRefreshExpiration:86400,
+    jwtExpiration:3600,
+    jwtRefreshExpiration:86400,
     //ตัวเทส
-    jwtExpiration: 60,
-    jwtRefreshExpiration: 120,
+    // jwtExpiration: 60,
+    // jwtRefreshExpiration: 120,
     }
